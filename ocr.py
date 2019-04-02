a72ae59edf5eaa9906fa58e3f16e10a0618c24be
chiefwithbeef <210036@ndhs.org>
2019-04-02 13:56:00 -0700

    removed blank output

--- ocr.py
+++ ocr.py
@@ -1,4 +1,3 @@
-from tesserocr import PyTessBaseAPI
 from pytesseract import *
 import cv2
 from PIL import Image
@@ -15,13 +14,21 @@ while True:
 	ret, frame = cam.read()
 	colorImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-	# Display the resulting frame
-	scan = cv2.imshow("webcam", colorImage)
+	# Display the rgb frame
+	colorScan = cv2.imshow("color camera", colorImage)
 
 	grayscaleImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-	tessFeed = pytesseract.image_to_boxes(colorImage)
-	print pytesseract.image_to_string(colorImage)
+	# display the grayscale image
+	grayscaleScan = cv2.imshow("grayscale camera", grayscaleImage)
+
+	tessFeed = pytesseract.image_to_boxes(grayscaleImage)
+
+	output = pytesseract.image_to_string(grayscaleImage)
+	if(output == ""):
+		print "nothing seen"
+	else:
+		print output, tessFeed
 
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
